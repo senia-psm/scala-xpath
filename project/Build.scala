451932fdd5fbaa8212381ee8d3667fa10af51f2d
senia-psm <seniapsm@gmail.com>
2013-01-29 11:16:33 +0400

    rename root build

--- project/Build.scala
+++ project/Build.scala
@@ -1,7 +1,7 @@
 import sbt._
 import Keys._
 
-object Sirc extends Build {
+object ScalaXPath extends Build {
   lazy val parser = Project(id = "xpath-parser", base = file("parser")) aggregate(model) dependsOn(model)
   lazy val model = Project(id = "xpath-model", base = file("model"))
 }
